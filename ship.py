fdb32cd49df31538e8b8939d8d5841be953f1fab
james-strom <james.stromnes@gmail.com>
2021-02-19 12:00:21 +0200

    second commit

--- ship.py
+++ ship.py
@@ -1,10 +1,13 @@
 import pygame
 
-class Ship:
+from pygame.sprite import Sprite
+
+class Ship(Sprite):
 	"""A class to manage the ship."""
 
 	def __init__(self, ai_game):
 		"""Initialize the ship and set its starting position."""
+		super().__init__()
 		self.screen = ai_game.screen
 		self.screen_rect = ai_game.screen.get_rect()
 		self.settings = ai_game.settings
@@ -12,6 +15,7 @@ class Ship:
 		# Load the ship image and get its rect.
 		self.image = pygame.image.load('images/ship.bmp')
 		self.rect = self.image.get_rect()
+		
 		# Start each new ship at the bottom center of the screen.
 		self.rect.midbottom = self.screen_rect.midbottom
 
@@ -35,3 +39,8 @@ class Ship:
 	def blitme(self):
 		"""Draw the ship at its current location."""
 		self.screen.blit(self.image, self.rect)
+
+	def center_ship(self):
+		"""Center the ship on the screen."""
+		self.rect.midbottom = self.screen_rect.midbottom
+		self.x = float(self.rect.x)
